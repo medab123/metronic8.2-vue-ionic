0fd1c5b3f1e4578b8313059eda35636aa7e39112
Elabidi mohammed <mohammed.elabidi123@gmail.com>
2023-10-29 11:20:31 +0100

    add ionic to metronic ionic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
-
+import { createPinia } from 'pinia';
+import { Tooltip } from "bootstrap";
 import { IonicVue } from '@ionic/vue';
+
+import ElementPlus from "element-plus";
+import i18n from "@/core/plugins/i18n";
+
+import ApiService from "@/core/services/ApiService";
+import { initApexCharts } from "@/core/plugins/apexcharts";
+import { initInlineSvg } from "@/core/plugins/inline-svg";
+import { initVeeValidate } from "@/core/plugins/vee-validate";
+import { initKtIcon } from "@/core/plugins/keenthemes";
+
+import "@/core/plugins/prismjs";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -23,10 +35,26 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+const pinia = createPinia();
+
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
-  
+  .use(pinia)
+  .use(router)
+  .use(ElementPlus);
+
+ApiService.init(app);
+initApexCharts(app);
+initInlineSvg(app);
+initKtIcon(app);
+initVeeValidate();
+
+app.use(i18n);
+app.directive("tooltip", (el) => {
+  new Tooltip(el);
+});
+
+
 router.isReady().then(() => {
   app.mount('#app');
 });
